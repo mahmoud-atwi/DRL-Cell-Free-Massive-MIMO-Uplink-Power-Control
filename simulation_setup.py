b0db7d28d1b41bd46d5f72811818b17cf6190145
Mahmoud Atwi <matwi@icloud.com>
2023-12-19 00:19:35 +0400

    add loopback variables to maintain same environment during benchmarking

--- simulation_setup.py
+++ simulation_setup.py
@@ -1,10 +1,10 @@
-import torch
-import numpy as np
-
-from scipy.linalg import sqrtm
 from math import sqrt
 
-from gpu_acc_se import compute_se_cf_ul
+import numpy as np
+import torch
+from scipy.linalg import sqrtm
+
+from compute_spectral_efficiency import compute_se_cf_ul
 from helper_functions import db2pow
 from pathloss_threeslope import path_loss_three_slope
 
@@ -12,52 +12,62 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def CF_mMIMO_Env(L, K, tau_p, max_power, UEs_power, APs_positions, UEs_positions, square_length, decorr, sigma_sf,
-                 noise_variance_dbm, delta):
-    gain_over_noise_db = torch.zeros((L, K), device=device)
-    distances_to_UE = torch.zeros((L, K), device=device)
+                 noise_variance_dbm, delta, pilot_index_loopback=None, beta_val_loopback=None):
 
-    # Compute alternative AP and UE locations by using wrap around
-    wrap_horizontal = torch.tile(torch.tensor([-square_length, 0, square_length]), (3, 1)).to(device)
-    wrap_vertical = wrap_horizontal.T
-    wrap_locations = (wrap_horizontal + 1j * wrap_vertical).T.flatten()
+    if beta_val_loopback is None:
+        gain_over_noise_db = torch.zeros((L, K), device=device)
+        distances_to_UE = torch.zeros((L, K), device=device)
 
-    wrapped_AP_positions = torch.tile(APs_positions.flatten(), (len(wrap_locations), 1)).T + torch.tile(wrap_locations,
-                                                                                                        (L, 1))
-    wrapped_UE_positions = torch.tile(UEs_positions.flatten(), (len(wrap_locations), 1)).T + torch.tile(wrap_locations,
-                                                                                                        (K, 1))
+        # Compute alternative AP and UE locations by using wrap around
+        wrap_horizontal = torch.tile(torch.tensor([-square_length, 0, square_length]), (3, 1)).to(device)
+        wrap_vertical = wrap_horizontal.T
+        wrap_locations = (wrap_horizontal + 1j * wrap_vertical).T.flatten()
 
-    shadow_corr_matrix_APs = torch.zeros((L, L), device=device)
-    shadow_corr_matrix_UEs = torch.zeros((K, K), device=device)
+        wrapped_AP_positions = torch.tile(APs_positions.flatten(), (len(wrap_locations), 1)).T + torch.tile(
+            wrap_locations,
+            (L, 1))
+        wrapped_UE_positions = torch.tile(UEs_positions.flatten(), (len(wrap_locations), 1)).T + torch.tile(
+            wrap_locations,
+            (K, 1))
 
-    for l in range(L):
-        distance_to_AP = torch.min(torch.abs(wrapped_AP_positions - APs_positions[l]), dim=1).values
-        shadow_corr_matrix_APs[:, l] = 2 ** (-1 * distance_to_AP / decorr)
+        shadow_corr_matrix_APs = torch.zeros((L, L), device=device)
+        shadow_corr_matrix_UEs = torch.zeros((K, K), device=device)
 
-    for k in range(K):
-        distance_to_UE = torch.min(torch.abs(wrapped_UE_positions - UEs_positions[k]), dim=1).values
-        shadow_corr_matrix_UEs[:, k] = 2 ** (-1 * distance_to_UE / decorr)
+        for l in range(L):
+            distance_to_AP = torch.min(torch.abs(wrapped_AP_positions - APs_positions[l]), dim=1).values
+            shadow_corr_matrix_APs[:, l] = 2 ** (-1 * distance_to_AP / decorr)
 
-    # Generate shadow fading realizations
-    a = sigma_sf * sqrtm(shadow_corr_matrix_APs.cpu()) @ np.random.randn(L, )
-    b = sigma_sf * sqrtm(shadow_corr_matrix_UEs.cpu()) @ np.random.randn(K, )
+        for k in range(K):
+            distance_to_UE = torch.min(torch.abs(wrapped_UE_positions - UEs_positions[k]), dim=1).values
+            shadow_corr_matrix_UEs[:, k] = 2 ** (-1 * distance_to_UE / decorr)
 
-    a = torch.from_numpy(a.real).to(device)
-    b = torch.from_numpy(b.real).to(device)
+        # Generate shadow fading realizations
+        a = sigma_sf * sqrtm(shadow_corr_matrix_APs.cpu()) @ np.random.randn(L, )
+        b = sigma_sf * sqrtm(shadow_corr_matrix_UEs.cpu()) @ np.random.randn(K, )
 
-    # Compute distances and channel gain
-    for k in range(K):
-        distance_to_UE, position_indices = torch.min(
-            torch.abs(wrapped_AP_positions - torch.tile(UEs_positions[k], wrapped_AP_positions.shape)), dim=1)
-        distances_to_UE[:, k] = distance_to_UE
+        a = torch.from_numpy(a.real).to(device)
+        b = torch.from_numpy(b.real).to(device)
 
-        gain_over_noise_db[:, k] = path_loss_three_slope(distance_to_UE) - noise_variance_dbm
+        # Compute distances and channel gain
+        for k in range(K):
+            distance_to_UE, position_indices = torch.min(
+                torch.abs(wrapped_AP_positions - torch.tile(UEs_positions[k], wrapped_AP_positions.shape)), dim=1)
+            distances_to_UE[:, k] = distance_to_UE
 
-        # Add shadow fading over 50 meters
-        mask = distance_to_UE > 50
-        gain_over_noise_db[mask, k] += sqrt(delta) * a[mask] + sqrt(1 - delta) * b[k]
+            gain_over_noise_db[:, k] = path_loss_three_slope(distance_to_UE) - noise_variance_dbm
 
-    pilot_index_CF = (torch.randperm(K) % tau_p)
-    beta_val = db2pow(gain_over_noise_db)
+            # Add shadow fading over 50 meters
+            mask = distance_to_UE > 50
+            gain_over_noise_db[mask, k] += sqrt(delta) * a[mask] + sqrt(1 - delta) * b[k]
+
+        beta_val = db2pow(gain_over_noise_db)
+    else:
+        beta_val = torch.from_numpy(beta_val_loopback).to(device)
+
+    if pilot_index_loopback is None:
+        pilot_index_CF = (torch.randperm(K) % tau_p)
+    else:
+        pilot_index_CF = torch.from_numpy(pilot_index_loopback).to(device)
 
     # Run greedy algorithm to find optimal pilot allocation in cell-free network
     tau_c = tau_p + 1
@@ -106,4 +116,4 @@ def CF_mMIMO_Env(L, K, tau_p, max_power, UEs_power, APs_positions, UEs_positions
 
     B_k = torch.sum(beta_val, dim=0)
 
-    return B_k, signal_CF, interference_CF, pilot_index_CF, APs_positions, UEs_positions
+    return B_k, signal_CF, interference_CF, pilot_index_CF, beta_val, APs_positions, UEs_positions
