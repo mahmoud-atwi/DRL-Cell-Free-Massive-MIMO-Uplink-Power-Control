ae5fac33a5126a977e567ceec108c960d87a9a2e
Mahmoud Atwi <matwi@icloud.com>
2023-12-19 00:22:18 +0400

    add power optimization algorithms as methods

--- single_input_env/env.py
+++ single_input_env/env.py
@@ -1,11 +1,10 @@
 import gymnasium as gym
 import numpy as np
 import torch
-
-from copy import deepcopy
 from gymnasium import spaces
 
 from helper_functions import calc_SINR
+from power_optimization import power_opt_maxmin, power_opt_prod_sinr, power_opt_sum_rate
 from simulation_setup import CF_mMIMO_Env
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -52,12 +51,14 @@ class CFmMIMOEnv(gym.Env):
         Reset the state of the environment to an initial state
         """
         # Initialize/reset the state (B_K values)
-        self.state, init_signal_Cf, init_interference_CF = self.initialize_B_K()
+        self.state, init_signal_CF, init_interference_CF, pilot_index_CF, beta_val_CF = self.initialize_B_K()
 
         info = dict()
 
-        info['init_signal'] = init_signal_Cf
+        info['init_signal'] = init_signal_CF
         info['init_interference'] = init_interference_CF
+        info['init_pilot_index'] = pilot_index_CF
+        info['init_beta_val'] = beta_val_CF
 
         return self.state, info
 
@@ -93,20 +94,26 @@ class CFmMIMOEnv(gym.Env):
         return updated_B_k, reward, done, truncated, info
 
     def initialize_B_K(self):
-        """
-        Initialize the B_K values for each user.
-        """
-        init_B_k, signal_CF, interference_CF, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p, self.max_power,
-                                                                self.initial_power, self.APs_positions,
-                                                                self.UEs_positions, self.square_length, self.decorr,
-                                                                self.sigma_sf,
-                                                                self.noise_variance_dbm, self.delta)
 
-        init_B_k_np = init_B_k.cpu().numpy()
-        signal_CF_np = signal_CF.cpu().numpy()
-        interference_CF_np = interference_CF.cpu().numpy()
+        UE_init_locations = torch.rand(self.K, dtype=torch.complex64, device=device) * self.square_length
 
-        return init_B_k_np, signal_CF_np, interference_CF_np
+        init_B_k, signal_CF, interference_CF, pilot_index, beta_val, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p,
+                                                                                       self.max_power,
+                                                                                       self.initial_power,
+                                                                                       self.APs_positions,
+                                                                                       UE_init_locations,
+                                                                                       self.square_length, self.decorr,
+                                                                                       self.sigma_sf,
+                                                                                       self.noise_variance_dbm,
+                                                                                       self.delta)
+
+        init_B_k_np = init_B_k.detach().cpu().numpy()
+        signal_CF_np = signal_CF.detach().cpu().numpy()
+        interference_CF_np = interference_CF.detach().cpu().numpy()
+        pilot_index_np = pilot_index.detach().cpu().numpy()
+        beta_val_np = beta_val.detach().cpu().numpy()
+
+        return init_B_k_np, signal_CF_np, interference_CF_np, pilot_index_np, beta_val_np
 
     def update_B_K(self):
         """
@@ -117,9 +124,9 @@ class CFmMIMOEnv(gym.Env):
                                                                    self.UEs_positions, self.square_length, self.decorr,
                                                                    self.sigma_sf, self.noise_variance_dbm, self.delta)
 
-        updated_B_k_np = updated_B_k.cpu().numpy()
-        signal_CF_np = signal_CF.cpu().numpy()
-        interference_CF_np = interference_CF.cpu().numpy()
+        updated_B_k_np = updated_B_k.detach().cpu().numpy()
+        signal_CF_np = signal_CF.detach().cpu().numpy()
+        interference_CF_np = interference_CF.detach().cpu().numpy()
 
         return updated_B_k_np, signal_CF_np, interference_CF_np
 
@@ -130,6 +137,116 @@ class CFmMIMOEnv(gym.Env):
         SINR = calc_SINR(self.UEs_power, signal, interference)
         r = np.sum(np.log2(1 + SINR))
         return float(r)
+
+    def calculate(self, signal, interference, action, lagging_SE, pilot_index, beta_val):
+        """
+        Apply action (adjust UL power) and calculate next state (B_k values) without updating the state
+        """
+        info = dict()
+        new_B_k = self.state
+        # Adjust UL power based on action
+        min_power, max_power = self.power_range
+        rescaled_action = ((action + 1) / 2) * (max_power - min_power) + min_power
+
+        self.UEs_power = rescaled_action  # Action is the new UL power levels
+
+        if lagging_SE:
+            info['signal'] = signal
+            info['interference'] = interference
+            info['predicted_power'] = rescaled_action
+
+        if not lagging_SE:
+            # Recalculate new B_k, signal and interference based on the new UL power
+            new_B_k, new_signal, new_interference, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p, self.max_power,
+                                                                     self.UEs_power, self.APs_positions,
+                                                                     self.UEs_positions, self.square_length,
+                                                                     self.decorr,
+                                                                     self.sigma_sf, self.noise_variance_dbm, self.delta,
+                                                                     pilot_index, beta_val)
+
+            info['signal'] = new_signal.detach().cpu().numpy()
+            info['interference'] = new_interference.detach().cpu().numpy()
+            info['predicted_power'] = rescaled_action
+
+        return new_B_k, info
+
+    def maxmin_algo(self, signal, interference, max_power, prelog_factor, lagging_SE, pilot_index, beta_val):
+
+        info = dict()
+        new_B_k = self.state
+
+        _, opt_power = power_opt_maxmin(signal, interference, max_power, prelog_factor, return_SE=False)
+
+        if lagging_SE:
+            info['signal'] = signal
+            info['interference'] = interference
+            info['predicted_power'] = opt_power
+
+        if not lagging_SE:
+            new_B_k, new_signal, new_interference, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p, self.max_power,
+                                                                     opt_power, self.APs_positions,
+                                                                     self.UEs_positions, self.square_length,
+                                                                     self.decorr,
+                                                                     self.sigma_sf, self.noise_variance_dbm, self.delta,
+                                                                     pilot_index, beta_val)
+
+            info['signal'] = new_signal.detach().cpu().numpy()
+            info['interference'] = new_interference.detach().cpu().numpy()
+            info['predicted_power'] = opt_power
+
+        return new_B_k, info
+
+    def maxprod_algo(self, signal, interference, max_power, prelog_factor, lagging_SE, pilot_index, beta_val):
+
+        info = dict()
+        new_B_k = self.state
+
+        _, opt_power = power_opt_prod_sinr(signal, interference, max_power, prelog_factor, return_SE=False)
+
+        if lagging_SE:
+            info['signal'] = signal
+            info['interference'] = interference
+            info['predicted_power'] = opt_power
+
+        if not lagging_SE:
+            new_B_k, new_signal, new_interference, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p, self.max_power,
+                                                                     opt_power, self.APs_positions,
+                                                                     self.UEs_positions, self.square_length,
+                                                                     self.decorr,
+                                                                     self.sigma_sf, self.noise_variance_dbm, self.delta,
+                                                                     pilot_index, beta_val)
+
+            info['signal'] = new_signal.detach().cpu().numpy()
+            info['interference'] = new_interference.detach().cpu().numpy()
+            info['predicted_power'] = opt_power
+
+        return new_B_k, info
+
+    def maxsumrate_algo(self, signal, interference, max_power, prelog_factor, lagging_SE, pilot_index, beta_val):
+
+        info = dict()
+        new_B_k = self.state
+
+        _, opt_power = power_opt_sum_rate(signal, interference, max_power, prelog_factor, return_SE=False)
+
+        if lagging_SE:
+            info['signal'] = signal
+            info['interference'] = interference
+            info['predicted_power'] = opt_power
+
+        if not lagging_SE:
+            new_B_k, new_signal, new_interference, *_ = CF_mMIMO_Env(self.L, self.K, self.tau_p, self.max_power,
+                                                                     opt_power, self.APs_positions,
+                                                                     self.UEs_positions, self.square_length,
+                                                                     self.decorr,
+                                                                     self.sigma_sf, self.noise_variance_dbm, self.delta,
+                                                                     pilot_index, beta_val)
+
+            info['signal'] = new_signal.detach().cpu().numpy()
+            info['interference'] = new_interference.detach().cpu().numpy()
+            info['predicted_power'] = opt_power
+
+        return new_B_k, info
 
     def close(self):
         pass
