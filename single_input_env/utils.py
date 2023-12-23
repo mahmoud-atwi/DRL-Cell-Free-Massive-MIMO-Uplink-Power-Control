35b67704a341fe6a07b9cd2a7d921d2a34146656
Mahmoud Atwi <matwi@icloud.com>
2023-12-23 13:35:28 +0400

    add "TrialEvalCallback" Class

--- single_input_env/utils.py
+++ single_input_env/utils.py
@@ -1,7 +1,11 @@
 import importlib
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Type, Optional
+
+import optuna
 
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.vec_env import VecEnv
 
 
 def get_class_by_name(name: str) -> Type:
@@ -26,7 +30,7 @@ def get_class_by_name(name: str) -> Type:
 
 def get_callback_list(hyperparams: Dict[str, Any]) -> List[BaseCallback]:
     """
-    Get one or more Callback class specified as a hyper-parameter
+    Get one or more Callback class specified as a hyperparameter
     "callback".
     e.g.
     callback: stable_baselines3.common.callbacks.CheckpointCallback
@@ -73,3 +77,46 @@ def get_callback_list(hyperparams: Dict[str, Any]) -> List[BaseCallback]:
             callbacks.append(callback_class(**kwargs))
 
     return callbacks
+
+
+class TrialEvalCallback(EvalCallback):
+    """
+    Callback used for evaluating and reporting a trial.
+    """
+
+    def __init__(
+            self,
+            eval_env: VecEnv,
+            trial: optuna.Trial,
+            n_eval_episodes: int = 5,
+            eval_freq: int = 10000,
+            deterministic: bool = True,
+            verbose: int = 2,
+            best_model_save_path: Optional[str] = None,
+            log_path: Optional[str] = None,
+    ) -> None:
+        super().__init__(
+            eval_env=eval_env,
+            n_eval_episodes=n_eval_episodes,
+            eval_freq=eval_freq,
+            deterministic=deterministic,
+            verbose=verbose,
+            best_model_save_path=best_model_save_path,
+            log_path=log_path,
+        )
+        self.trial = trial
+        self.eval_idx = 0
+        self.is_pruned = False
+
+    def _on_step(self) -> bool:
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            super()._on_step()
+            self.eval_idx += 1
+            # report best or report current ?
+            # report num_timesteps or elapsed time ?
+            self.trial.report(self.last_mean_reward, self.eval_idx)
+            # Prune trial if needed
+            if self.trial.should_prune():
+                self.is_pruned = True
+                return False
+        return True
