62c398154e5210143c652a7e2eb3b73e5fc2000c
Mahmoud Atwi <matwi@icloud.com>
2023-12-24 10:38:32 +0400

    reformat

--- simulation_para.py
+++ simulation_para.py
@@ -8,7 +8,6 @@ min_power = 0
 max_power = 100
 initial_power = 100
 
-
 # Three-Slope Simulation Parameters
 square_length = 1000  # meters
 bandwidth = 20e6  # bandwidth in Hz
@@ -19,6 +18,7 @@ delta = 0.5  # shadow fading decorrelation parameter
 decorr = 100  # decorrelation distance in meters
 antenna_spacing = 0.5  # antenna spacing in wavelengths
 angular_sd_deg = 15  # angular standard deviation in degrees
+
 # for mobility
 speed_range = [0.5, 5]
 max_pause_time = 5
