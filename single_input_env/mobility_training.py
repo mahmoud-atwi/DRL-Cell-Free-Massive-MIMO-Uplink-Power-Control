7db7e1ad9f8480472ba29c1647bede102fffcdd2
Mahmoud Atwi <matwi@icloud.com>
2023-12-29 03:27:55 +0400

    update

--- single_input_env/mobility_training.py
+++ single_input_env/mobility_training.py
@@ -1,18 +1,16 @@
 import sys
+import yaml
 from pathlib import Path
-
-import torch
-import torch.optim as optim
 
 from stable_baselines3.common.env_checker import check_env
 from torch import cuda
 from torch.backends import mps
 
 from algos import ALGOS
+from _utils import generate_ap_locations, generate_ue_locations
 
-from mobility_env import MobilityCFmMIMOEnv
-from simulation_para import L, K, tau_p, min_power, max_power, initial_power, square_length, decorr, sigma_sf, \
-    noise_variance_dbm, delta
+from env import MobilityCFmMIMOEnv
+from simulation_para import L, K, square_length
 
 if sys.platform == "darwin":  # Check if macOS
     device = "mps" if mps.is_available() else "cpu"
@@ -30,43 +28,50 @@ if device == "mps":
 else:
     _device = device
 
-AP_locations = torch.rand(L, dtype=torch.complex64, device=_device) * square_length
-UE_initial_locations = torch.rand(K, dtype=torch.complex64, device=_device) * square_length
+yaml_path = "../sac.yaml"
 
-env = MobilityCFmMIMOEnv(L=L, K=K, tau_p=tau_p, initial_power=initial_power, min_power=min_power, max_power=max_power,
-                         APs_positions=AP_locations, UEs_positions=UE_initial_locations, square_length=square_length,
-                         decorr=decorr, sigma_sf=sigma_sf, noise_variance_dbm=noise_variance_dbm, delta=delta)
+with open(yaml_path) as f:
+    hyperparams_dict = yaml.safe_load(f)
 
-config = {
-    "algo": "SAC",
-    "policy_type": "MlpPolicy",
-    "total_timesteps": 10000,
-    "env_name": env.__class__.__name__,
-    "learning_rate": 5e-4,
-    "batch_size": 128,
-    "optimizer_class": optim.SGD,
-    "net_arch": [128, 256, 128],
-}
+area_bounds = (0, square_length, 0, square_length)
+AP_locations = generate_ap_locations(L, 100, area_bounds)
+UE_initial_locations = generate_ue_locations(K, area_bounds)
+
+verbose = 1
+seed = 0
+
+# config = {
+#     "algo": "SAC",
+#     "policy_type": "MlpPolicy",
+#     "total_timesteps": 10000,
+#     "env_name": env.__class__.__name__,
+#     "learning_rate": 5e-4,
+#     "batch_size": 128,
+#     "optimizer_class": optim.SGD,
+#     "net_arch": [128, 256, 128],
+# }
 
 
-# It will check your custom environment and output additional warnings if needed
+env = MobilityCFmMIMOEnv(APs_positions=AP_locations, UEs_positions=UE_initial_locations, UEs_mobility=True,)
+
+# Check the environment before training
 check_env(env, warn=True)
 
-model = ALGOS[config["algo"]](
-    config["policy_type"],
-    env,
-    learning_rate=config["learning_rate"],
-    batch_size=config["batch_size"],
-    policy_kwargs=dict(net_arch=config["net_arch"]),
-    verbose=1,
-    device=device,
-)
+meanSE_20 = "MobilityCFmMIMOEnv-v0"
+meanSE = "MobilityCFmMIMOEnv-v1"
+minSE = "MobilityCFmMIMOEnv-v2"
+sumSE = "MobilityCFmMIMOEnv-v3"
+geo_meanSE = "MobilityCFmMIMOEnv-v4"
 
+
+kwargs = dict()
+kwargs.update(hyperparams_dict[meanSE_20])
+model = ALGOS[kwargs['algo']](env=env, seed=seed, verbose=verbose, device=device, **kwargs,)
 
 print(model.policy)
 
-model.learn(config["total_timesteps"], progress_bar=True, )
-
-model.save("SAC_CFmMIMO_SGD[MPS][M]")
-
-del model
+# model.learn(config["total_timesteps"], progress_bar=True, )
+#
+# model.save("SAC_CFmMIMO_SGD[MPS][M]")
+#
+# del model
