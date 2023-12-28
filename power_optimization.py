108fac73ab80d5e39a0ed0513d28795291ef22a9
Mahmoud Atwi <matwi@icloud.com>
2023-12-29 03:26:22 +0400

    update to support functions writen using numpy

--- power_optimization.py
+++ power_optimization.py
@@ -1,21 +1,12 @@
-import time
-import torch
 import numpy as np
 
 from scipy.optimize import minimize
 
-from helper_functions import feasibility_problem_cvx_np, compute_prod_sinr
-from compute_spectral_efficiency import compute_se_np
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+from _utils import feasibility_problem_cvx, compute_prod_sinr
+from compute_spectral_efficiency import compute_se
 
 
-def power_opt_maxmin(signal, interference, max_power, prelog_factor, return_SE=False):
-
-    if not isinstance(signal, np.ndarray):
-        signal = signal.cpu().numpy()
-    if not isinstance(interference, np.ndarray):
-        interference = interference.cpu().numpy()
+def power_opt_maxmin(signal, interference, max_power, prelog_factor, return_spectral_efficiency=False):
 
     K = signal.shape[0]
 
@@ -28,9 +19,9 @@ def power_opt_maxmin(signal, interference, max_power, prelog_factor, return_SE=F
 
     while np.linalg.norm(upper_rate - lower_rate) > delta:
         candidate_rate = (upper_rate + lower_rate) / 2
-        candidate_SINR = (2 ** candidate_rate) - 1
+        candidate_sinr = (2 ** candidate_rate) - 1
 
-        feasible, p_solution = feasibility_problem_cvx_np(signal, interference, max_power, K, candidate_SINR)
+        feasible, p_solution = feasibility_problem_cvx(signal, interference, max_power, K, candidate_sinr)
 
         if feasible:
             lower_rate = candidate_rate
@@ -38,14 +29,14 @@ def power_opt_maxmin(signal, interference, max_power, prelog_factor, return_SE=F
         else:
             upper_rate = candidate_rate
 
-    if return_SE:
-        SE = compute_se_np(signal, interference, p_best, prelog_factor)
+    if return_spectral_efficiency:
+        SE = compute_se(signal, interference, p_best, prelog_factor)
         return SE, p_best
     else:
         return None, p_best
 
 
-def power_opt_prod_sinr(signal, interference, max_power, prelog_factor, return_SE=False):
+def power_opt_prod_sinr(signal, interference, max_power, prelog_factor, return_spectral_efficiency=False):
 
     if not isinstance(signal, np.ndarray):
         signal = signal.cpu().numpy()
@@ -65,14 +56,14 @@ def power_opt_prod_sinr(signal, interference, max_power, prelog_factor, return_S
     rhoBest = result.x
 
     # Compute SE with the initial solution
-    if return_SE:
-        SE = compute_se_np(signal, interference, rhoSolution, prelog_factor)
+    if return_spectral_efficiency:
+        SE = compute_se(signal, interference, rhoSolution, prelog_factor)
         return SE, rhoBest
     else:
         return None, rhoBest
 
 
-def power_opt_sum_rate(signal, interference, max_power, prelog_factor, return_SE=False):
+def power_opt_sum_rate(signal, interference, max_power, prelog_factor, return_spectral_efficiency=False):
 
     if not isinstance(signal, np.ndarray):
         signal = signal.cpu().numpy()
@@ -88,54 +79,16 @@ def power_opt_sum_rate(signal, interference, max_power, prelog_factor, return_SE
     bounds = [(0, max_power)] * K
 
     def objective_func(rho):
-        return -np.sum(compute_se_np(signal, interference, rho, prelog_factor))
+        return -np.sum(compute_se(signal, interference, rho, prelog_factor))
 
     result = minimize(objective_func, rhoSolution, bounds=bounds, method='L-BFGS-B')
 
     rhoBest = result.x
 
     # Compute the SEs
-    if return_SE:
-        SE = compute_se_np(signal, interference, rhoBest, prelog_factor)
+    if return_spectral_efficiency:
+        SE = compute_se(signal, interference, rhoBest, prelog_factor)
         return SE, rhoBest
     else:
         return None, rhoBest
-
-
-if __name__ == '__main__':
-    _K = 40
-
-    _signal = torch.randn(_K, dtype=torch.float32, device=device)
-    _interference = torch.randn(_K, _K, dtype=torch.float32, device=device)
-    _max_power = 100
-    _prelog_factor = 1
-
-    print('power_opt_maxmin started')
-    start = time.time()
-    _SE, _p_best = power_opt_maxmin(_signal, _interference, _max_power, _prelog_factor, return_SE=True)
-    print(f'power_opt_maxmin Done | Time elapsed: {time.time() - start}')
-    # print SE value and shape
-    print(f'SE: {_SE}')
-    print(f'SE: {_SE.shape}')
-    # print p_best value and shape
-    print(f'p_best: {_p_best}')
-    print(f'p_best: {_p_best.shape}')
-
-    _SE, _rhoBest = power_opt_prod_sinr(_signal, _interference, _max_power, _prelog_factor, return_SE=True)
-    # print SE value and shape
-    print(f'_SE: {_SE}')
-    print(f'_SE: {_SE.shape}')
-    # print rhoBest value and shape
-    print(f'_rhoBest: {_rhoBest}')
-    print(f'_rhoBest: {_rhoBest.shape}')
-
-    print('power_opt_sum_rate started')
-    start = time.time()
-    _SE, _rhoBest = power_opt_sum_rate(_signal, _interference, _max_power, _prelog_factor, return_SE=True)
-    print(f'power_opt_sum_rate Done | Time elapsed: {time.time() - start}')
-    # print SE value and shape
-    print(f'_SE: {_SE}')
-    print(f'_SE: {_SE.shape}')
-    # print rhoBest value and shape
-    print(f'_rhoBest: {_rhoBest}')
-    print(f'_rhoBest: {_rhoBest.shape}')
+    
