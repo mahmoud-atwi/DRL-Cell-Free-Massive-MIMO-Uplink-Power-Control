db10ac8e83c4147ade8de935311ee886a823c957
Mahmoud Atwi <matwi@icloud.com>
2024-01-05 03:34:54 +0400

    Add additional models

--- _options.py
+++ _options.py
@@ -6,6 +6,21 @@ from torch.optim.adam import Adam
 from torch.optim import SGD
 
 
+# MobilityCFmMIMOEnv-delta_cf_se-v0
+# MobilityCFmMIMOEnv-exp_delta_cf_se_clip-v0
+# MobilityCFmMIMOEnv-exp_delta_cf_se_clip-v1
+# MobilityCFmMIMOEnv-log_delta_cf_se-v0
+# MobilityCFmMIMOEnv-relative_cf_se-v0
+# MobilityCFmMIMOEnv-exp_relative_cf_se_clip-v0
+# MobilityCFmMIMOEnv-log_relative_cf_se-v0
+# MobilityCFmMIMOEnv-delta_sinr-v0
+# MobilityCFmMIMOEnv-exp_delta_sinr_clip-v0
+# MobilityCFmMIMOEnv-log_delta_sinr-v0
+# MobilityCFmMIMOEnv-relative_sinr-v0
+# MobilityCFmMIMOEnv-exp_relative_sinr_clip-v0
+# MobilityCFmMIMOEnv-log_relative_sinr-v0
+
+
 def select_reward_option():
     # List of available reward options
     reward_options = {
@@ -13,16 +28,42 @@ def select_reward_option():
         "2": "Geometric Mean SE",
         "3": "Mean SE",
         "4": "Min SE",
-        "5": "Sum SE"
+        "5": "Sum SE",
+        "6": "Delta CF SE",
+        "7": "Exp Delta CF SE Clip",
+        "8": "Exp Delta CF SE Clip V1",
+        "9": "Log Delta CF SE",
+        "10": "Relative CF SE",
+        "11": "Exp Relative CF SE Clip",
+        "12": "Log Relative CF SE",
+        "13": "Delta SINR",
+        "14": "Exp Delta SINR Clip",
+        "15": "Log Delta SINR",
+        "16": "Relative SINR",
+        "17": "Exp Relative SINR Clip",
+        "18": "Log Relative SINR",
     }
 
     # Map of user selection to environment name
     env_map = {
-        "1": "MobilityCFmMIMOEnv-ch_capacity-v0",
-        "2": "MobilityCFmMIMOEnv-geo_mean_se-v0",
-        "3": "MobilityCFmMIMOEnv-mean_se-v0",
-        "4": "MobilityCFmMIMOEnv-min_se-v0",
-        "5": "MobilityCFmMIMOEnv-sum_se-v0"
+        "1": ("MobilityCFmMIMOEnv-ch_capacity-v0", "channel_capacity", None, None),
+        "2": ("MobilityCFmMIMOEnv-geo_mean_se-v0", "geo_mean_se", None, None),
+        "3": ("MobilityCFmMIMOEnv-mean_se-v0", "mean_se", None, None),
+        "4": ("MobilityCFmMIMOEnv-min_se-v0", "min_se", None, None),
+        "5": ("MobilityCFmMIMOEnv-sum_se-v0", "sum_se", None, None),
+        "6": ("MobilityCFmMIMOEnv-delta_cf_se-v0", None, "delta", "cf_se"),
+        "7": ("MobilityCFmMIMOEnv-exp_delta_cf_se_clip-v0", None, "exp_delta_clip", "cf_se"),
+        "8": ("MobilityCFmMIMOEnv-exp_delta_cf_se_clip-v1", None, "exp_delta_clip", "cf_se"),
+        "9": ("MobilityCFmMIMOEnv-log_delta_cf_se-v0", None, "log_delta", "cf_se"),
+        "10": ("MobilityCFmMIMOEnv-relative_cf_se-v0", None, "relative", "cf_se"),
+        "11": ("MobilityCFmMIMOEnv-exp_relative_cf_se_clip-v0", None, "exp_relative_clip", "cf_se"),
+        "12": ("MobilityCFmMIMOEnv-log_relative_cf_se-v0", None, "log_relative", "cf_se"),
+        "13": ("MobilityCFmMIMOEnv-delta_sinr-v0", None, "delta", "sinr"),
+        "14": ("MobilityCFmMIMOEnv-exp_delta_sinr_clip-v0", None, "exp_delta_clip", "sinr"),
+        "15": ("MobilityCFmMIMOEnv-log_delta_sinr-v0", None, "log_delta", "sinr"),
+        "16": ("MobilityCFmMIMOEnv-relative_sinr-v0", None, "relative", "sinr"),
+        "17": ("MobilityCFmMIMOEnv-exp_relative_sinr_clip-v0", None, "exp_relative_clip", "sinr"),
+        "18": ("MobilityCFmMIMOEnv-log_relative_sinr-v0", None, "log_relative", "sinr"),
     }
 
     while True:
