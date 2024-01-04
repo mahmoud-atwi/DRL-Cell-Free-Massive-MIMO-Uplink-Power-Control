8f7fce40916954faa5d8805824bc57f529230916
Mahmoud Atwi <matwi@icloud.com>
2024-01-04 09:10:37 +0400

    Add new reward methods

--- env.py
+++ env.py
@@ -1,15 +1,18 @@
+import warnings
+import numpy as np
+import gymnasium as gym
+
+from gymnasium import spaces
+from collections import deque
+from scipy.stats import gmean
 from typing import Tuple, Optional
 
-import gymnasium as gym
-import numpy as np
-from gymnasium import spaces
-
 import simulation_para as sim_para
-from _utils import generate_ap_locations, generate_ue_locations, calc_sinr
-from compute_spectral_efficiency import compute_se
-from power_optimization import power_opt_maxmin, power_opt_prod_sinr, power_opt_sum_rate
 from random_waypoint import random_waypoint
 from simulation_setup import cf_mimo_simulation
+from compute_spectral_efficiency import compute_se
+from _utils import generate_ap_locations, generate_ue_locations, calc_sinr
+from power_optimization import power_opt_maxmin, power_opt_prod_sinr, power_opt_sum_rate
 
 
 class MobilityCFmMIMOEnv(gym.Env):
@@ -74,7 +77,46 @@ class MobilityCFmMIMOEnv(gym.Env):
         self.relocate_APs_on_reset = kwargs.get('relocate_AP_on_reset', False)
         self.relocate_UEs_on_reset = kwargs.get('relocate_UEs_on_reset', False)
         self.area_bounds = (0, self.square_length, 0, self.square_length)
-        self.reward_method = kwargs.get('reward_method', 'min_se')
+        self.reward_method = kwargs.get('reward_method', None)
+        self.temporal_reward_method = kwargs.get('temporal_reward_method', None)
+
+        # raise error if both reward_method and temporal_reward_method are provided
+        if self.reward_method and self.temporal_reward_method:
+            raise ValueError(
+                """
+                Only one reward method can be used at a time. 
+                Please choose either reward_method or temporal_reward_method.
+                
+                reward_method options: 
+                    1) channel_capacity
+                    2) min_se
+                    3) mean_se
+                    4) sum_se
+                    5) geo_mean_se
+                    6) cf_min_se
+                    7) cf_mean_se
+                    8) cf_sum_se
+                
+                temporal_reward_method options:
+                    1) delta
+                    2) relative
+                    3) exp_delta_clip
+                    4) exp_relative_clip
+                    5) log_delta
+                    6) log_relative'
+                """
+            )
+
+        methods = ["delta", "relative", "exp_delta_clip", "exp_relative_clip", "log_delta", "log_relative"]
+        if self.temporal_reward_method not in methods:
+            raise ValueError(f"Invalid temporal reward method. select one of: {methods}")
+
+        # Temporal parameters
+        self.temporal_reward_operation = kwargs.get('temporal_reward_operation', 'mean')
+        self.temporal_reward_max = kwargs.get('temporal_reward_max', 10)
+        self.temporal_data = kwargs.get('temporal_data', "cf_se")
+        self.temporal_window_size = kwargs.get('temporal_window_size', 10)
+        self.temporal_history = deque(maxlen=self.temporal_window_size)
 
         # Define action space (continuous UL power levels for each UE)
         self.action_space = spaces.Box(low=-1, high=1, shape=(self.K,), dtype=np.float32)
@@ -84,14 +126,35 @@ class MobilityCFmMIMOEnv(gym.Env):
                                             dtype=np.float32)
 
         # Initial state (B_K values)
-        self.state, *_ = self.initialize_state()
+        self.state, _init_signal, _init_interference, _init_cf_se, *_ = self.initialize_state()
+
+        if self.temporal_data == "cf_se":
+            self.temporal_history.append(_init_cf_se)
+        elif self.temporal_data == "sinr":
+            _init_ues_power = np.full(self.K, self.initial_power)
+            _init_sinr = calc_sinr(_init_ues_power, _init_signal, _init_interference)
+            self.temporal_history.append(_init_sinr)
+        else:
+            warnings.warn(f"Temporal data {self.temporal_data} not supported. Falling back to spectral efficiency.")
+            self.temporal_history.append(_init_cf_se)
 
     def reset(self, **kwargs) -> Tuple[np.ndarray, dict]:
         # Initialize/reset the state (B_K values)
-        _init_Beta_K, _init_signal, _init_interference, _init_SE, _init_pilot_index, _init_beta_val = (
+        _init_Beta_K, _init_signal, _init_interference, _init_cf_se, _init_pilot_index, _init_beta_val = (
             self.initialize_state())
 
         self.state = _init_Beta_K
+
+        if self.temporal_data == "cf_se":
+            self.temporal_history.append(_init_cf_se)
+        elif self.temporal_data == "sinr":
+            _init_ues_power = np.full(self.K, self.initial_power)
+            _init_sinr = calc_sinr(_init_ues_power, _init_signal, _init_interference)
+            self.temporal_history.append(_init_sinr)
+        else:
+            warnings.warn(f"Temporal data {self.temporal_data} not supported. Falling back to spectral efficiency.")
+            self.temporal_data = "cf_se"
+            self.temporal_history.append(_init_cf_se)
 
         _info = dict()
 
@@ -114,10 +177,17 @@ class MobilityCFmMIMOEnv(gym.Env):
             self.UEs_positions = self.update_ue_positions()
 
         # Recalculate B_k based on the new UL power
-        _updated_Beta_K, _updated_signal, _updated_interference, _cf_spectral_efficiency = self.update_state()
+        _updated_Beta_K, _updated_signal, _updated_interference, _updated_cf_se = self.update_state()
 
         # Calculate reward
-        _reward = self.calculate_reward(_updated_signal, _updated_interference, _cf_spectral_efficiency)
+        _reward = self.calculate_reward(_updated_signal, _updated_interference, _updated_cf_se)
+
+        # add to temporal history
+        if self.temporal_data == "cf_se":
+            self.temporal_history.append(_updated_cf_se)
+        elif self.temporal_data == "sinr":
+            _updated_sinr = calc_sinr(self.UEs_power, _updated_signal, _updated_interference)
+            self.temporal_history.append(_updated_sinr)
 
         done = False
         truncated = False
@@ -172,38 +242,116 @@ class MobilityCFmMIMOEnv(gym.Env):
     def calculate_reward(self, signal: np.ndarray, interference: np.ndarray,
                          cf_spectral_efficiency: np.ndarray) -> float:
         # Method to calculate the reward
-        if self.reward_method == "channel_capacity":
-            _SINR = calc_sinr(self.UEs_power, signal, interference)
-            return np.sum(np.log2(1 + _SINR)).astype(np.float32)
-        elif self.reward_method == "min_se":
-            SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
-            return np.min(SE).astype(np.float32)
-        elif self.reward_method == "mean_se":
-            SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
-            return np.mean(SE).astype(np.float32)
-        elif self.reward_method == "sum_se":
-            SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
-            return np.sum(SE).astype(np.float32)
-        elif self.reward_method == "geo_mean_se":
-            SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
-            # Calculate geometric mean
-            geometric_mean = np.prod(SE) ** (1.0 / len(SE))
-            return geometric_mean.astype(np.float32)
-        elif self.reward_method == "cf_min_se":
-            return np.min(cf_spectral_efficiency).astype(np.float32)
-        elif self.reward_method == "cf_mean_se":
-            return np.mean(cf_spectral_efficiency).astype(np.float32)
-        elif self.reward_method == "cf_geo_mean_se":
-            # Calculate geometric mean
-            geometric_mean = np.prod(cf_spectral_efficiency) ** (1.0 / len(cf_spectral_efficiency))
-            return geometric_mean.astype(np.float32)
-        elif self.reward_method == "cf_sum_se":
-            return np.sum(cf_spectral_efficiency).astype(np.float32)
+        if self.reward_method is None:
+            if self.temporal_reward_method is None:
+                warnings.warn(
+                    "No reward method provided. temporal reward will be used with 'exp_relative_clip' method.")
+                self.temporal_reward_method = "exp_relative_clip"
+
+            if self.temporal_data == "cf_se":
+                return self._calculate_temporal_reward(cf_spectral_efficiency)
+            elif self.temporal_data == "sinr":
+                _sinr = calc_sinr(self.UEs_power, signal, interference)
+                return self._calculate_temporal_reward(_sinr)
+            else:
+                warnings.warn(
+                    f"Temporal data {self.temporal_data} not supported. Falling back to spectral efficiency.")
+                self.temporal_data = "cf_se"
+                return self._calculate_temporal_reward(cf_spectral_efficiency)
         else:
-            # fallback to min_se in case the reward_method provided is not supported and raise warning
-            print("Warning: reward method not supported. Falling back to min_se.")
-            SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
-            return np.min(SE)
+            if self.reward_method == "channel_capacity":
+                _SINR = calc_sinr(self.UEs_power, signal, interference)
+                return np.sum(np.log2(1 + _SINR)).astype(np.float32)
+            elif self.reward_method == "min_se":
+                SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
+                return np.min(SE).astype(np.float32)
+            elif self.reward_method == "mean_se":
+                SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
+                return np.mean(SE).astype(np.float32)
+            elif self.reward_method == "sum_se":
+                SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
+                return np.sum(SE).astype(np.float32)
+            elif self.reward_method == "geo_mean_se":
+                SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
+                # Calculate geometric mean
+                geometric_mean = np.prod(SE) ** (1.0 / len(SE))
+                return geometric_mean.astype(np.float32)
+            elif self.reward_method == "cf_min_se":
+                return np.min(cf_spectral_efficiency).astype(np.float32)
+            elif self.reward_method == "cf_mean_se":
+                return np.mean(cf_spectral_efficiency).astype(np.float32)
+            elif self.reward_method == "cf_geo_mean_se":
+                # Calculate geometric mean
+                geometric_mean = np.prod(cf_spectral_efficiency) ** (1.0 / len(cf_spectral_efficiency))
+                return geometric_mean.astype(np.float32)
+            elif self.reward_method == "cf_sum_se":
+                return np.sum(cf_spectral_efficiency).astype(np.float32)
+            else:
+                # fallback to min_se in case the reward_method provided is not supported and raise warning
+                print("Warning: reward method not supported. Falling back to min_se.")
+                SE = compute_se(signal, interference, self.UEs_power, sim_para.prelog_factor)
+                return np.min(SE)
+
+    def _calculate_temporal_reward(self, current: np.ndarray) -> float:
+        # Method to calculate temporal reward
+        methods = ["delta", "relative", "exp_delta_clip", "exp_relative_clip", "log_delta", "log_relative"]
+        if self.temporal_reward_method not in methods:
+            raise ValueError(f"Invalid temporal reward method. select one of: {methods}")
+        _method = self.temporal_reward_method
+
+        operations = {
+            'min': np.min,
+            'max': np.max,
+            'mean': np.mean,
+            'sum': np.sum,
+            'gmean': gmean
+        }
+
+        if self.temporal_reward_operation is None or self.temporal_reward_operation not in operations:
+            raise ValueError(f"Invalid operation. Choose from {list(operations.keys())}")
+        _operation = self.temporal_reward_operation
+
+        # if history is empty, return 0.0
+        if not self.temporal_history:
+            return 0.0
+
+        avg_history = np.mean(self.temporal_history, axis=0)
+        avg_history = np.where(avg_history == 0, 1e-6, avg_history)
+
+        if _method == "delta":
+            return operations[_operation](current - avg_history).astype(np.float32)
+
+        elif _method == "relative":
+            return operations[_operation](current / avg_history).astype(np.float32)
+
+        elif _method == "exp_delta_clip":
+            delta = current - avg_history
+            scaling_factor = 1.0 / (self.temporal_reward_max - 1.0)
+            exp_delta = np.exp(delta * scaling_factor)
+            # Clipping the rewards
+            temporal_reward = operations[_operation](np.clip(exp_delta, None, self.temporal_reward_max))
+            return temporal_reward.astype(np.float32)
+
+        elif _method == "exp_relative_clip":
+            ratio = current / avg_history
+            exp_ratio = np.exp(ratio - 1)
+            # Clipping the rewards
+            max_reward = self.temporal_reward_max
+            temporal_reward = operations[_operation](np.clip(exp_ratio, None, max_reward))
+            return temporal_reward.astype(np.float32)
+
+        elif _method == "log_delta":
+            delta = current - avg_history
+            constant_offset = 1e-6
+            log_delta = np.log(np.abs(delta) + constant_offset)
+            temporal_reward = operations[_operation](log_delta)
+            return temporal_reward.astype(np.float32)
+
+        elif _method == "log_relative":
+            ratio = current / avg_history
+            log_ratio = np.log(ratio + 1e-6)
+            temporal_reward = operations[_operation](log_ratio)
+            return temporal_reward.astype(np.float32)
 
     def calculate(self, signal: np.ndarray, interference: np.ndarray, action: np.ndarray,
                   lagging_spectral_efficiency: bool, pilot_index: np.ndarray, beta_val: np.ndarray) -> dict:
