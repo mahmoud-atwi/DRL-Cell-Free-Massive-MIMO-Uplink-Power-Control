9b7918df3e6ff62086629350052a10ed79ff80ae
Mahmoud Atwi <matwi@icloud.com>
2024-01-05 03:41:15 +0400

    re-write as class

--- training.py
+++ training.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import warnings
+from copy import copy
 from datetime import datetime
 from pathlib import Path
 
 from stable_baselines3.common.env_checker import check_env
 from torch import cuda
 from torch.backends import mps
+from torch.optim import SGD, Adam
 
 from _options import select_reward_option, load_hyperparameters, get_config, get_hyperparameters
 from _utils import generate_ap_locations, generate_ue_locations
@@ -13,103 +16,108 @@ from algos import ALGOS
 from env import MobilityCFmMIMOEnv as CFmMIMOEnv
 from simulation_para import L, K, square_length
 
-if sys.platform == 'darwin':  # Check if macOS
-    device = "mps" if mps.is_available() else "cpu"
-else:
-    device = "cuda" if cuda.is_available() else "cpu"
 
-verbose = 0
-seed = 0
+class RLModelTrainer:
+    def __init__(self, seed=0, verbose=0):
+        self.seed: int = seed
+        self.verbose: int = verbose
+        self.optim_cls: str = ''
+        self.env_reward: str = ''
+        self.device: str = ''
+        self.config: dict = dict()
+        self.initialize()
 
-config = get_config()
+    @staticmethod
+    def _set_device():
+        if sys.platform == 'darwin':
+            return "mps" if mps.is_available() else "cpu"
+        else:
+            return "cuda" if cuda.is_available() else "cpu"
 
-print(f'Algo: {config["algo"]}')
-print(f'Optimizer: {config["optimizer_class"].__name__}')
-print(f'total_timesteps: {config["total_timesteps"]}')
+    def initialize(self):
+        self.device = self._set_device()
+        self._setup_directories()
+        self.config = get_config()
+        self._load_hyperparams()
+        self._setup_environment()
 
-yaml_file = get_hyperparameters(config)
+    def _setup_directories(self):
+        self.log_dir = Path('logs')
+        self.models_dir = 'models'
 
-# Reward (1): Channel Capacity  --> MobilityCFmMIMOEnv-ch_capacity-v0
-# Reward (2): Geometric Mean SE --> MobilityCFmMIMOEnv-geo_mean_se-v0
-# Reward (3): Mean SE           --> MobilityCFmMIMOEnv-mean_se-v0
-# Reward (4): Min SE            --> MobilityCFmMIMOEnv-min_se-v0
-# Reward (5): Sum SE            --> MobilityCFmMIMOEnv-sum_se-v0
+    def _load_hyperparams(self):
+        self.yaml_file = get_hyperparameters(self.config)
+        env_name, self.reward_method, self.temporal_reward_method, self.temporal_data = select_reward_option()
+        self.env_reward = self.temporal_reward_method if self.temporal_reward_method is not None else self.reward_method
+        self.hyperparams = load_hyperparameters(self.yaml_file, env_name)
+        self._set_optimizer_class()
 
-# load pre-tuned hyperparams
-_env_reward = select_reward_option()
-_hyperparams = load_hyperparameters(yaml_file, _env_reward)
-_hyperparams["policy_kwargs"]['optimizer_class'] = config["optimizer_class"]
+    def _set_optimizer_class(self):
+        try:
+            self.optim_cls = copy(self.hyperparams["policy_kwargs"]['optimizer_class'])
+        except KeyError:
+            warnings.warn("No optimizer_class specified in policy_kwargs, defaulting to SGD.")
+            self.optim_cls = "SGD"
 
-# logs directory
-log_dir = Path('logs')
-log_path = os.path.join(log_dir, f'{_env_reward}-{config["algo"]}-{str(config["optimizer_class"].__name__)}')
-os.makedirs(os.path.dirname(log_path), exist_ok=True)
+        if self.optim_cls is None or self.optim_cls.upper() == "SGD":
+            self.hyperparams["policy_kwargs"]['optimizer_class'] = SGD
+        elif self.optim_cls.upper() == "ADAM":
+            self.hyperparams["policy_kwargs"]['optimizer_class'] = Adam
 
-# distribute APs and UEs
-area_bounds = (0, square_length, 0, square_length)
-APs_positions = generate_ap_locations(L, 100, area_bounds)
-UEs_positions = generate_ue_locations(K, area_bounds)
+    def _setup_environment(self):
+        area_bounds = (0, square_length, 0, square_length)
+        self.APs_positions = generate_ap_locations(L, 100, area_bounds)
+        self.UEs_positions = generate_ue_locations(K, area_bounds)
 
-if _env_reward == "MobilityCFmMIMOEnv-ch_capacity-v0":
-    reward_method = 'channel_capacity'
-elif _env_reward == "MobilityCFmMIMOEnv-geo_mean_se-v0":
-    reward_method = 'geo_mean_se'
-elif _env_reward == "MobilityCFmMIMOEnv-mean_se-v0":
-    reward_method = 'mean_se'
-elif _env_reward == "MobilityCFmMIMOEnv-min_se-v0":
-    reward_method = 'min_se'
-elif _env_reward == "MobilityCFmMIMOEnv-sum_se-v0":
-    reward_method = 'sum_se'
-else:
-    raise ValueError("Invalid reward option selected.")
+        self.env = CFmMIMOEnv(
+            APs_positions=self.APs_positions,
+            UEs_positions=self.UEs_positions,
+            UEs_mobility=True,
+            reward_method=self.reward_method,
+            temporal_reward_method=self.temporal_reward_method,
+            temporal_data=self.temporal_data,
+        )
+        check_env(self.env, warn=True)
 
-# directory to save models
-models_dir = 'models'
-model_path = os.path.join(models_dir, f'{config["algo"]}-{str(config["optimizer_class"].__name__)}-{reward_method}')
-os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    def train(self):
+        model = ALGOS[self.config["algo"]](
+            policy=self.hyperparams["policy"],
+            env=self.env,
+            learning_rate=self.hyperparams["learning_rate"],
+            buffer_size=self.hyperparams["buffer_size"],
+            learning_starts=self.hyperparams["learning_starts"],
+            batch_size=self.hyperparams["batch_size"],
+            tau=self.hyperparams["tau"],
+            gamma=self.hyperparams["gamma"],
+            train_freq=self.hyperparams["train_freq"],
+            gradient_steps=self.hyperparams["gradient_steps"],
+            ent_coef=self.hyperparams["ent_coef"],
+            policy_kwargs=self.hyperparams["policy_kwargs"],
+            seed=self.seed,
+            verbose=self.verbose,
+            device=self.device,
+        )
+        log_path = self._get_log_path()
+        model.learn(total_timesteps=self.config["total_timesteps"], log_interval=1000, tb_log_name=log_path,
+                    progress_bar=True)
+        self._save_model(model)
+
+    def _get_log_path(self):
+        return os.path.join(self.log_dir, f'{self.config["algo"]}', f'{self.optim_cls.upper()}',
+                            f'{self.env_reward.upper()}', f'{self.temporal_data.upper()}')
+
+    def _save_model(self, model):
+        current_time = datetime.now().strftime("%Y%m%d%H%M")
+        model_name = (f'MODEL_{self.config["algo"]}_{self.optim_cls.upper()}_{self.env_reward.upper()}_'
+                      f'{self.temporal_data.upper()}_{current_time}')
+        save_path = os.path.join(self.models_dir, model_name)
+        model.save(save_path)
+        del model  # delete trained model from memory
 
 
-env = CFmMIMOEnv(
-    APs_positions=APs_positions,
-    UEs_positions=UEs_positions,
-    UEs_mobility=True,
-    reward_method=reward_method,
-)
+if __name__ == '__main__':
+    trainer = RLModelTrainer()
+    trainer.train()
 
-# check the custom environment
-check_env(env, warn=True)
 
-# model
-print(_hyperparams["policy_kwargs"])
-model = ALGOS[config["algo"]](
-    policy=_hyperparams["policy"],
-    env=env,
-    learning_rate=_hyperparams["learning_rate"],
-    buffer_size=_hyperparams["buffer_size"],
-    learning_starts=_hyperparams["learning_starts"],
-    batch_size=_hyperparams["batch_size"],
-    tau=_hyperparams["tau"],
-    gamma=_hyperparams["gamma"],
-    train_freq=_hyperparams["train_freq"],
-    gradient_steps=_hyperparams["gradient_steps"],
-    ent_coef=_hyperparams["ent_coef"],
-    policy_kwargs=_hyperparams["policy_kwargs"],
-    seed=seed,
-    verbose=verbose,
-    device=device,
-)
-
-print(f'model:{model.policy}')
-print(f'model optimizer:{model.policy.optimizer_class}')
-
-# train
-model.learn(total_timesteps=config["total_timesteps"], log_interval=1000, tb_log_name=log_path, progress_bar=True)
-
-# save
-current_time = datetime.now().strftime("%Y%m%d-%H%M")
-model_name = \
-    f'{env.__class__.__name__}_{config["algo"]}_{config["optimizer_class"].__name__}_{reward_method}_{current_time}'
-save_path = os.path.join(model_path, model_name)
-model.save(save_path)
-
-del model  # delete trained model from memory
+# 14 Exp Delta SINR Clip: failed
