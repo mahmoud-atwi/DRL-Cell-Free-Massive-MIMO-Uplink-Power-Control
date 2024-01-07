2e403251529077e289ce5b759e2d85942d3dd454
Mahmoud Atwi <matwi@icloud.com>
2024-01-07 16:21:29 +0400

    add ComputationalComplexityBenchmark class

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,5 @@
 import copy
+from time import time
 from typing import Any, Dict
 
 import numpy as np
@@ -289,7 +290,6 @@ class MultiModelBenchmark:
                 self.results[model_name]['SIGNALS'][:, i] = info['signal']
                 self.results[model_name]['CF_SEs'][:, i] = info['cf_spectral_efficiency']
                 self.results[model_name]['SINRs'][:, i] = info['sinr']
-                # self.results[model_name]['INTERFERENCEs'][:, :, i] = info['interference']
 
             self.UEs_LOCATIONS[:, i] = copy.deepcopy(self.ues_positions)
 
@@ -325,7 +325,6 @@ class MultiModelBenchmark:
                 self.MAXPROD_POWERs[:, i] = maxprod_info['optimized_power']
                 self.MAXPROD_SIGNALs[:, i] = maxprod_info['signal']
                 self.MAXPROD_SINRs[:, i] = maxprod_info['sinr']
-                # self.MAXPROD_INTERFERENCEs[:, :, i] = maxprod_info['interference']
 
             if self.include_sumrate:
                 sumrate_info = self.sumrate_env.maxsumrate_algo(self.sumrate_signal, self.sumrate_interference,
@@ -342,7 +341,6 @@ class MultiModelBenchmark:
                 self.SUMRATE_POWERs[:, i] = sumrate_info['optimized_power']
                 self.SUMRATE_SIGNALs[:, i] = sumrate_info['signal']
                 self.SUMRATE_SINRs[:, i] = sumrate_info['sinr']
-                # self.SUMRATE_INTERFERENCEs[:, :, i] = sumrate_info['interference']
 
         # Convert results to Pandas DataFrames
         results_df = {
@@ -351,19 +349,16 @@ class MultiModelBenchmark:
             'MAXMIN_SIGNALs': pd.DataFrame(self.MAXMIN_SIGNALs),
             'MAXMIN_CF_SEs': pd.DataFrame(self.MAXMIN_CF_SEs),
             'MAXMIN_SINRs': pd.DataFrame(self.MAXMIN_SINRs),
-            # 'MAXMIN_INTERFERENCEs': pd.DataFrame(self.MAXMIN_INTERFERENCEs.reshape(self.env.K, -1)),
             'MAXPROD_SEs': pd.DataFrame(self.MAXPROD_SEs),
             'MAXPROD_POWERs': pd.DataFrame(self.MAXPROD_POWERs),
             'MAXPROD_SIGNALs': pd.DataFrame(self.MAXPROD_SIGNALs),
             'MAXPROD_CF_SEs': pd.DataFrame(self.MAXPROD_CF_SEs),
             'MAXPROD_SINRs': pd.DataFrame(self.MAXPROD_SINRs),
-            # 'MAXPROD_INTERFERENCEs': pd.DataFrame(self.MAXPROD_INTERFERENCEs.reshape(self.env.K, -1)),
             'SUMRATE_SEs': pd.DataFrame(self.SUMRATE_SEs),
             'SUMRATE_POWERs': pd.DataFrame(self.SUMRATE_POWERs),
             'SUMRATE_SIGNALs': pd.DataFrame(self.SUMRATE_SIGNALs),
             'SUMRATE_CF_SEs': pd.DataFrame(self.SUMRATE_CF_SEs),
             'SUMRATE_SINRs': pd.DataFrame(self.SUMRATE_SINRs),
-            # 'SUMRATE_INTERFERENCEs': pd.DataFrame(self.SUMRATE_INTERFERENCEs.reshape(self.env.K, -1))
             'UEs_LOCATIONS': pd.DataFrame(self.UEs_LOCATIONS)
         }
 
@@ -372,5 +367,139 @@ class MultiModelBenchmark:
             for metric_name, values in metrics.items():
                 df_key = f"{model_name}_{metric_name}"
                 results_df[df_key] = pd.DataFrame(values.reshape(self.env.K, -1))
+
+        return results_df
+
+
+class ComputationalComplexityBenchmark:
+    """
+    Benchmark class to evaluate different models and classical power allocation algorithms .
+
+    Attributes:
+        models (Dict[str, Any]): A dictionary of predictive models used in the benchmark, keyed by model names.
+        env (Any): The environment for the models, containing parameters like number of users (K).
+        max_power (float): Maximum power limit for the algorithms.
+        prelog_factor (float): Pre-log factor used in spectral efficiency computation.
+        lagging_se (bool): Flag to indicate if lagging spectral efficiency is considered.
+        include_maxmin (bool): Flag to include max-min power allocation algorithm in the benchmark.
+        include_maxprod (bool): Flag to include max-product power allocation algorithm in the benchmark.
+        include_sumrate (bool): Flag to include sum-rate power allocation algorithm in the benchmark.
+        num_of_iterations (int): Number of iterations for the benchmark.
+        [various other attributes for storing results]
+        mobility (bool): Flag indicating if user mobility is considered in the environment.
+    """
+
+    def __init__(self, models: Dict[str, Any], env: Any, max_power: float = 100, prelog_factor: float = 1,
+                 lagging_se: bool = False, include_maxmin: bool = True, include_maxprod: bool = True,
+                 include_sumrate: bool = True, num_of_iterations: int = 100, mobility: bool = False):
+
+        self.models: Dict[str, Any] = models
+        self.env: Any = env
+        self.max_power: float = max_power
+        self.prelog_factor: float = prelog_factor
+        self.lagging_se: bool = lagging_se
+        self.include_maxmin: bool = include_maxmin
+        self.include_maxprod: bool = include_maxprod
+        self.include_sumrate: bool = include_sumrate
+        self.num_of_iterations: int = num_of_iterations
+        self.mobility: bool = mobility
+
+        self.results = {model_name: {
+            'DURATIONs': np.zeros((1, self.num_of_iterations)),
+        } for model_name in self.models}
+
+        self.models_env = {model_name: copy.deepcopy(self.env) for model_name in self.models}
+
+        self.maxmin_env = copy.deepcopy(self.env)
+        self.maxprod_env = copy.deepcopy(self.env)
+        self.sumrate_env = copy.deepcopy(self.env)
+
+        self.MAXMIN_DURATIONs = np.zeros((1, self.num_of_iterations))
+        self.MAXPROD_DURATIONs = np.zeros((1, self.num_of_iterations))
+        self.SUMRATE_DURATIONs = np.zeros((1, self.num_of_iterations))
+
+        self.init_obs, self.init_info = self.env.reset()
+        self.ues_positions = self.env.UEs_positions
+
+        self.obs = {model_name: copy.deepcopy(self.init_obs) for model_name in self.models}
+
+        init_signal = self.init_info['init_signal']
+        init_interference = self.init_info['init_interference']
+
+        self.maxmin_signal = copy.deepcopy(init_signal)
+        self.maxprod_signal = copy.deepcopy(init_signal)
+        self.sumrate_signal = copy.deepcopy(init_signal)
+
+        self.maxmin_interference = copy.deepcopy(init_interference)
+        self.maxprod_interference = copy.deepcopy(init_interference)
+        self.sumrate_interference = copy.deepcopy(init_interference)
+
+    def run(self, show_progress: bool = True):
+        """
+        Executes the benchmark for the specified number of iterations and returns the results as Pandas DataFrames.
+
+        Args:
+            show_progress (bool): If True, shows a progress bar during execution.
+
+        Returns:
+            Dict[str, pd.DataFrame]: A dictionary containing the results of the benchmark as Pandas DataFrames.
+        """
+
+        iterator = tqdm(range(self.num_of_iterations), desc="Running Benchmark") if show_progress else (
+            range(self.num_of_iterations))
+
+        for i in iterator:
+            if self.mobility:
+                self.ues_positions = self.env.update_ue_positions(self.ues_positions)
+            else:
+                self.ues_positions = None
+
+            for model_name, model in self.models.items():
+                _start_time = time()
+                action, _ = model.predict(self.obs[model_name], deterministic=True)
+                _end_time = time()
+                self.obs[model_name], info = self.models_env[model_name].simulate(action=action,
+                                                                                  ues_positions=self.ues_positions)
+                self.results[model_name]['DURATIONs'][:, i] = _end_time - _start_time
+
+            if self.include_maxmin:
+                maxmin_info = self.maxmin_env.maxmin_algo(self.maxmin_signal, self.maxmin_interference, self.max_power,
+                                                          self.ues_positions, self.prelog_factor, self.lagging_se, None,
+                                                          None)
+
+                self.maxmin_signal = maxmin_info['signal']
+                self.maxmin_interference = maxmin_info['interference']
+                self.MAXMIN_DURATIONs[:, i] = maxmin_info['duration']
+
+            if self.include_maxprod:
+                maxprod_info = self.maxprod_env.maxprod_algo(self.maxprod_signal, self.maxprod_interference,
+                                                             self.max_power, self.ues_positions, self.prelog_factor,
+                                                             self.lagging_se, None, None)
+
+                self.maxprod_signal = maxprod_info['signal']
+                self.maxprod_interference = maxprod_info['interference']
+                self.MAXPROD_DURATIONs[:, i] = maxprod_info['duration']
+
+            if self.include_sumrate:
+                sumrate_info = self.sumrate_env.maxsumrate_algo(self.sumrate_signal, self.sumrate_interference,
+                                                                self.max_power, self.ues_positions, self.prelog_factor,
+                                                                self.lagging_se, None, None)
+
+                self.sumrate_signal = sumrate_info['signal']
+                self.sumrate_interference = sumrate_info['interference']
+                self.SUMRATE_DURATIONs[:, i] = sumrate_info['duration']
+
+        # Convert results to Pandas DataFrames
+        results_df = {
+            'MAXMIN_DURATIONs': pd.DataFrame(self.MAXMIN_DURATIONs),
+            'MAXPROD_DURATIONs': pd.DataFrame(self.MAXPROD_DURATIONs),
+            'SUMRATE_DURATIONs': pd.DataFrame(self.SUMRATE_DURATIONs),
+        }
+
+        # Add results from predictive models
+        for model_name, metrics in self.results.items():
+            for metric_name, values in metrics.items():
+                df_key = f"{model_name}_{metric_name}"
+                results_df[df_key] = pd.DataFrame(values)
 
         return results_df
