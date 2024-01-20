bbc9c78cead15edb83da09d5b00964cb3eacc404
Mahmoud Atwi <matwi@icloud.com>
2024-01-21 01:19:57 +0400

    add hyperparams sampler for DDPG

--- hyperparams.py
+++ hyperparams.py
@@ -1,8 +1,10 @@
 import optuna
+import numpy as np
 from typing import Any, Dict
 
 from torch.optim.adam import Adam
 from torch.optim import SGD
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
 
 def sample_sac_params(trial: optuna.Trial) -> Dict[str, Any]:
@@ -55,6 +57,10 @@ def sample_sac_params(trial: optuna.Trial) -> Dict[str, Any]:
 
     OPTIMIZER = SGD
 
+    q_window = trial.suggest_categorical("q_window", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    # r_alpha = trial.suggest_float("r_alpha", 0.01, 10)
+    # r_beta = trial.suggest_float("r_beta", 0.01, 10)
+
     hyperparams = {
         "gamma": gamma,
         "learning_rate": learning_rate,
@@ -69,9 +75,80 @@ def sample_sac_params(trial: optuna.Trial) -> Dict[str, Any]:
         "policy_kwargs": dict(optimizer_class=OPTIMIZER, net_arch=net_arch),
     }
 
-    return hyperparams
+    extra_hyperparams = {
+        "temporal_window_size": q_window,
+        # "r_alpha": r_alpha,
+        # "r_beta": r_beta,
+    }
+
+    return hyperparams, extra_hyperparams
+
+
+def sample_ddpg_params(trial: optuna.Trial, n_actions: int = 32) -> Dict[str, Any]:
+    """
+    Sampler for DDPG hyperparams.
+
+    :param trial:
+    :param n_actions:
+    :return:
+    """
+    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1, log=True)
+    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 100, 128, 256, 512, 1024, 2048])
+    buffer_size = trial.suggest_categorical("buffer_size", [int(1e4), int(1e5), int(1e6)])
+    # Polyak coeff
+    tau = trial.suggest_categorical("tau", [0.001, 0.005, 0.01, 0.02, 0.05, 0.08])
+
+    train_freq = trial.suggest_categorical("train_freq", [1, 4, 8, 16, 32, 64, 128, 256, 512])
+    gradient_steps = train_freq
+
+    noise_type = trial.suggest_categorical("noise_type", ["ornstein-uhlenbeck", "normal", None])
+    noise_std = trial.suggest_float("noise_std", 0, 1)
+
+    # NOTE: Add "verybig" to net_arch when tuning HER (see TD3)
+    net_arch_type = trial.suggest_categorical("net_arch", ["small", "medium", "big", 'mixed', 'large'])
+    # activation_fn = trial.suggest_categorical('activation_fn', [nn.Tanh, nn.ReLU, nn.ELU, nn.LeakyReLU])
+
+    net_arch = {
+        "small": [64, 64],
+        "medium": [256, 256],
+        "big": [400, 300],
+        "mixed": [128, 256, 128],
+        "large": [256, 256, 256],
+        # "verybig": [512, 512, 512],
+    }[net_arch_type]
+
+    hyperparams = {
+        "gamma": gamma,
+        "tau": tau,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "buffer_size": buffer_size,
+        "train_freq": train_freq,
+        "gradient_steps": gradient_steps,
+        "policy_kwargs": dict(net_arch=net_arch),
+    }
+
+    if noise_type == "normal":
+        hyperparams["action_noise"] = NormalActionNoise(mean=np.zeros(n_actions), sigma=noise_std * np.ones(n_actions))
+    elif noise_type == "ornstein-uhlenbeck":
+        hyperparams["action_noise"] = OrnsteinUhlenbeckActionNoise(
+            mean=np.zeros(n_actions), sigma=noise_std * np.ones(n_actions)
+        )
+
+    # q_window = trial.suggest_categorical("q_window", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    q_window = 10
+
+    extra_hyperparams = {
+        "temporal_window_size": q_window,
+        # "r_alpha": r_alpha,
+        # "r_beta": r_beta,
+    }
+
+    return hyperparams, extra_hyperparams
 
 
 HYPERPARAMS_SAMPLER = {
     "SAC": sample_sac_params,
+    "DDPG": sample_ddpg_params,
 }
